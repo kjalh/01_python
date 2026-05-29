00fa6041d6e9c9f495302dc0c71486cb055607a5
이희승 <heseung0895@naver.com>
2026-05-30 03:15:28 +0900

    여기까지 너무 졸림

--- 자료구조/queue(FIFO).py
+++ 자료구조/queue(FIFO).py
@@ -4,15 +4,14 @@ i = 0
 
 def enqueue(): # 데이터 추가
     global i
-    if len(queue) >= last:
+
+    if queue[size-1] != None:
         return print("\n큐가 꽉 차있음, 데이터 삭제 바람\n")
     else:
         data = input("넣고 싶은 데이터 입력: ")
-        queue.append(data)
-        #queue[i] = data
+        queue[i] = data
+
         i += 1
-
-
         print(queue)
         print('성공적으로 입력이 들어감')
 
@@ -20,24 +19,30 @@ def enqueue(): # 데이터 추가
 
 def dequeue(): # 데이터 삭제
     global i
-    if len(queue)<1:
+    if queue[0] == None:
         return print("큐가 비어있음")
     
     print(f"1번 제거 {queue[0]}")
 
 
-    for j in range(i-1): # 4    0 1 2 3
+    for j in range(0, i-1): # 4    0 1 2 3
+        queue[j] =  None
         queue[j] = queue[j+1]
-        #queue[:] = queue[:-1]
-    
+        queue[j + 1] = None
+
+
+        # queue[j] = None
+        # queue[j] = queue[j+1]
+        # queue[j+1] = None
+
     i -= 1
     print(queue)
 
 
 
-last = int(input("큐의 크기를 정해라: "))
+size = int(input("큐의 크기: "))
 
-queue = []
+queue = [None] * size 
 
 
 while True:
