ba4c11b81634119d98008d38bb3f34e0c3e460ca
이희승 <heseung0895@naver.com>
2026-05-30 19:00:02 +0900

    111111

--- 자료구조/stack(LIFO).py
+++ 자료구조/stack(LIFO).py
@@ -1,19 +1,69 @@
-
+i = 0 # 데이터 넣는 위치 결정
 
 def push(): # 데이터 추가
-    pass
+    global i
+    if stack[size-1] != None:   #   스택 전부 채워져 있는지 확인
+        return print("\n스택 꽉 차있음, 데이터 삭제 바람\n")
+    else:
+        data = input("넣고 싶은 데이터 입력: ")
+        stack[i] = data # 데이터 넣기
+        i += 1  # 데이터 위치 1 증가
 
+        if i >= size:
+            i -= 1
 
 
 def pop():  # 데이터 삭제
-    pass
+    global i
+
+    if stack[0] == None:
+        print("스택 비어 있음")
+
+    stack[i] = None
+    i -= 1
+
+    if i <= 0:
+        i = 0
+    
 
 
 
 
 
 
-num = int(input())
+
+
+
+
+
+
+
+size = int(input("큐의 크기: "))
+
+stack = [None] * size 
+
+while True:
+    print("-------------------------메뉴-----------------------------")
+    print("종료: 0")
+    print("추가: 넣")
+    print("삭제: 빼")
+
+    data = input("\n입력: ")
+
+    if data == '0':
+        break
+
+    elif data == "넣":
+        push()
+        print()
+    elif data == "빼":
+        pop()
+        print()
+    else: 
+        print("다시 입력 바람") 
+        print()
+    
+    print(stack)  # 스택 확인 없어도 됨
 
 
 
