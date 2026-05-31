2f0376aa2b692486b90e05babc0b21af7483fbaa
이희승 <heseung0895@naver.com>
2026-05-31 17:50:15 +0900

    설명 쓰기

--- 자료구조/re_queue(FIFO).py
+++ 자료구조/re_queue(FIFO).py
@@ -1,37 +1,36 @@
 class Queue:   
-    def __init__(self, size):
-        self.size = size
-        self.queue = [None] * (size + 1)
-        self.idx = 0
+    def __init__(self, size):       # 생성자 
+        self.size = size            # 큐 크기 받기
+        self.queue = [None] * (size + 1) # 큐 크기 지정 + 1은 하나 크게 만들어서 빼기 쉽게 
+        self.idx = 0                # 이 위치에 넣기
 
     
 
-
     def enqueue(self, in_data):
-        if self.idx >= self.size:
-            self.idx = self.size
+        if self.idx >= self.size:       # 빈 큐인지 확인
+            self.idx = self.size        # 더이상 증가 안 되게 
             return print("\n입력 실패 꽉 참")
         
-        self.queue[self.idx] = in_data 
-        self.idx += 1
+        self.queue[self.idx] = in_data  # 큐에 데이터 넣기
+        self.idx += 1                   # 다음 큐에 넣기 위해 idx(위치) 1wmdrk
 
-        print(self.queue)
+        print(self.queue)               # 큐에 제대로 들어갔는지 시각적 자료
 
 
 
     def dequeue(self): 
-        if self.idx == 0:
+        if self.idx == 0:               # 빈 큐 확인
             return print("빈 큐")
 
-        j = 0
-        while True if self.queue[j] != None else False:
-            self.queue[j] = self.queue[j+1]
-            j += 1
+        j = 0                           # 반복문에 쓰일
+        while True if self.queue[j] != None else False:   # 해당 위치에 값이 있어야 True
+            self.queue[j] = self.queue[j+1]   # 뒤에 있는 값을 하나씩 앞으로 이동
+            j += 1                      # j로 인덱스 확인
 
-        self.idx -= 1
-        if self.idx <= -1:
+        self.idx -= 1                   # 넣을 위치 -1
+        if self.idx <= -1:              # 넣을 위치가 -1 이하가 되지않도록 확인
             self.idx = 0
-        print(self.queue)
+        print(self.queue)               # 큐에서 제대로 빠졌는지 시각적 자료
 
 
 size = int(input("큐의 크기: "))
